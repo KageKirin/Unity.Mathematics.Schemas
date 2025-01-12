c062167fa1c30963f3fe64c567bddbf844cad083
KageKirin <kagekirin@gmail.com>
2025-01-12 12:34:31 +0900

    feat: implement Dummy functionality unit test

---
+++ Tests/Functionality/Dummy.cs
@@ -0,0 +1,13 @@
+using Unity.Mathematics.Schemas;
+using Xunit;
+
+namespace Unity.Mathematics.Schemas.Functionality.Tests;
+
+public class Dummy
+{
+    [Fact]
+    public void Test()
+    {
+        Assert.True(true);
+    }
+}
